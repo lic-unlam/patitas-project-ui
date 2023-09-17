b13e148cd6680bfebb66443cdb0fb7a4f7c02e47
lic-unlam <vasquez.gustavo@outlook.com>
2023-09-17 00:42:24 -0300

    Agregado formulario para dejar un comentario para el refugio.

--- src/components/refugio/secciones/Comentarios.jsx
+++ src/components/refugio/secciones/Comentarios.jsx
@@ -1,14 +1,25 @@
-import React, { useEffect } from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import CustomAlert from '../../layout/CustomAlert';
 
 const Comentarios = (props) => {
+    const [descripcionEstrella, setDescripcionEstrella] = useState({ nroEstrella: 0, leyenda: "" });
+    const [ contenidoAlert, setContenidoAlert ] = useState({
+        mostrar: false,
+        tipo: "",
+        mensaje: ""
+    });
+
+    // valoraciones: excelente, muy recomendable, buen lugar, poco recomendable, un desastre
+    const leyendasAlReves = ['Un desastre', 'Poco recomendable', 'Buen lugar', 'Muy recomendable', 'Excelente'];
+
     useEffect(() => {
         document.title = props.title.concat(' - ', window.$title);
     }, []);
 
     function comments_loop() {
         let comments = [];
-        // valoraciones: excelente, muy recomendable, buen lugar, poco recomendable, un desastre
         const estrellas = [5,4,3,2,1];
         const leyendas = ['Excelente', 'Muy recomendable', 'Buen lugar', 'Poco recomendable', 'Un desastre'];
 
@@ -41,7 +52,7 @@ const Comentarios = (props) => {
                                 <img className="img-fluid nav-profile-picture" src="/img/default_profile_picture.png" alt="profile_picture"/>
                             </div>
                             <div className="col-auto">
-                                <Link to="/usuarios/10"><p className="comment-name">cosme_fulanito</p></Link>
+                                <Link to="/usuarios/10"><p className="comment-name">adoptante.test</p></Link>
                                 <p className="comment-date">27/10/2022 21:47</p>
                             </div>
                             <div>
@@ -62,8 +73,66 @@ const Comentarios = (props) => {
         return comments;
     }
 
+    const mostrarDescripcionEstrella = (event) => {
+        const nroEstrella = event.target.dataset.nro;
+        setDescripcionEstrella({ nroEstrella: nroEstrella, leyenda: leyendasAlReves[nroEstrella - 1] });
+    }
+
+    const limpiarDescripcionEstrella = () => setDescripcionEstrella("");
+
+    const agregarComentario = (event) => {
+        event.preventDefault();
+        
+        setContenidoAlert({
+            mostrar: true,
+            tipo: "success",
+            mensaje: <span><strong>¡Gracias por dejar tu comentario!</strong> Se mostrará una vez que haya sido aprobado por un administrador.</span>
+        });
+    }
+
+    const ocultarAlert = () => {
+        setContenidoAlert({ mostrar: false });
+    }
+
     return (
-        <div className="row">{comments_loop()}</div>
+        <>
+        {contenidoAlert.mostrar && <CustomAlert tipo={contenidoAlert.tipo} ocultarAlert={ocultarAlert}>{contenidoAlert.mensaje}</CustomAlert>}
+        <div id="comentarios_wrapper" className="row justify-content-center">
+            <div className="col">
+                <div className="card">
+                    <h5 className="card-header fw-bold">Cuentános tu experiencia</h5>
+                    <div className="card-body">
+                        <h5 className="card-title">¿Sabías que tus comentarios son una forma muy importante de apoyar a los refugios?</h5>
+                        <p className="card-text">Al hacerlo, estás ayudando a mejorar su trabajo y atraer más visitantes. Te invitamos a que dejes un comentario y valoración sobre tu experiencia en el camino de la adopción responsable:</p>
+                        <div className="row align-items-center">
+                            <div className="col-12 col-md-9">
+                                <form onSubmit={agregarComentario}>
+                                    <div className="mb-3">
+                                        <i className={`bi ${ descripcionEstrella.nroEstrella >= 1 ? "bi-star-fill" : "bi-star" } pe-2 fs-4`} data-nro="1" onMouseOver={mostrarDescripcionEstrella} onMouseLeave={limpiarDescripcionEstrella}></i>
+                                        <i className={`bi ${ descripcionEstrella.nroEstrella >= 2 ? "bi-star-fill" : "bi-star" } pe-2 fs-4`} data-nro="2" onMouseOver={mostrarDescripcionEstrella} onMouseLeave={limpiarDescripcionEstrella}></i>
+                                        <i className={`bi ${ descripcionEstrella.nroEstrella >= 3 ? "bi-star-fill" : "bi-star" } pe-2 fs-4`} data-nro="3" onMouseOver={mostrarDescripcionEstrella} onMouseLeave={limpiarDescripcionEstrella}></i>
+                                        <i className={`bi ${ descripcionEstrella.nroEstrella >= 4 ? "bi-star-fill" : "bi-star" } pe-2 fs-4`} data-nro="4" onMouseOver={mostrarDescripcionEstrella} onMouseLeave={limpiarDescripcionEstrella}></i>
+                                        <i className={`bi ${ descripcionEstrella.nroEstrella >= 5 ? "bi-star-fill" : "bi-star" } pe-2 fs-4`} data-nro="5" onMouseOver={mostrarDescripcionEstrella} onMouseLeave={limpiarDescripcionEstrella}></i>
+                                        <span className="ps-3">{descripcionEstrella.leyenda}</span>
+                                    </div>
+                                    <div className="mb-3">
+                                        <textarea id="comentario" name="comentario" className="form-control" placeholder="Máximo 300 caracteres..."></textarea>
+                                    </div>
+                                    <button type="submit" className="btn btn-dark">Dejar un comentario</button>
+                                </form>
+                            </div>
+                            <div className="col-12 col-md-3">
+                                <img className="img-fluid" width={200} src="https://img.freepik.com/vector-premium/lindo-gato-trabajando-portatil-escribiendo-papel-ilustracion-icono-dibujos-animados_138676-2267.jpg?w=2000" alt="gato_escribiendo" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row pt-4">
+                {comments_loop()}
+            </div>
+        </div>
+        </>
     );
 }
 
