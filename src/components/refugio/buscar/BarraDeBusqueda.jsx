6d3537279dcd870c854c0afb2af4ccddce85c823
lic-unlam <vasquez.gustavo@outlook.com>
2023-10-10 12:32:27 -0300

    Implementada la búsqueda de refugios con filttros.

--- src/components/refugio/buscar/BarraDeBusqueda.jsx
+++ src/components/refugio/buscar/BarraDeBusqueda.jsx
@@ -1,25 +1,77 @@
-export const BarraDeBusqueda = () => {
+import { useState, useEffect, useCallback } from "react";
+
+export const BarraDeBusqueda = (props) => {
+    const [ barrios, setBarrios ] = useState([]);
+    const setResultados = props.setResultados;
+
+    const cargarBarrios = useCallback(async () => {
+        const response = await fetch("https://localhost:7277/api/barrios", {
+            method: "GET"
+        });
+
+        if(!response.ok) {
+            throw new Error("Hubo un problema con la solicitud. Código: " + response.status);
+        }
+
+        const data = await response.json();
+        setBarrios(data);
+    }, []);
+
+    useEffect(() => {
+        cargarBarrios();
+    }, [cargarBarrios]);
+
+    const buscarRefugios = async (event) => {
+        event.preventDefault();
+        let nombre = event.target.nombre.value;
+        let barrio = event.target.barrio.value;
+
+        try {
+            let queryString = '';
+            
+            if(nombre)
+                queryString += `nombre=${nombre}`;
+
+            if(barrio)
+                queryString += queryString ? `&barrio=${barrio}` : `barrio=${barrio}`;
+
+            const response = await fetch(`https://localhost:7277/api/refugios/buscar${queryString && '?' + queryString}`, {
+                method: "GET"
+            });
+    
+            if(!response.ok)
+                throw new Error("Hubo un problema con la solicitud. Código: " + response.status);
+
+            const data = await response.json();
+            setResultados(data);
+        }
+        catch(error) {
+            console.log(error.message);
+        }
+    }
+
     return (
         <div id="barra_de_busqueda_wrapper">
             <h1>Buscador de refugios</h1>
             <div className="row justify-content-center">
                 <div className="col-12 col-md-6">
-                    <div className="input-group">
-                        <input id="shelter_search" type="text" className="form-control" placeholder="Escriba el nombre del refugio..." aria-label="Escriba el nombre del refugio"/>
-                        <button id="shelter_search_button" type="button" className="btn"><i className="bi bi-search"></i></button>
-                    </div>
-                    <div className="form-group row justify-content-center py-2">
-                        <label className="col-12 col-md-auto my-auto">Filtrar por barrio porteño:</label>
-                        <div className="col-7 col-md-4">
-                            <select id="barrio_selector" className="form-select form-select-sm" aria-label="Seleccionar barrio porteño">
-                                <option value="0" defaultValue>Todos</option>
-                                <option value="1">Recoleta</option>
-                                <option value="2">Congreso</option>
-                                <option value="3">Puerto Madero</option>
-                                <option value="4">Caballito</option>
-                            </select>
+                    <form id="buscador_form" onSubmit={buscarRefugios}>
+                        <div className="input-group">
+                            <input id="nombre" name="nombre" type="text" className="form-control" placeholder="Escriba el nombre del refugio..." aria-label="Escriba el nombre del refugio" autoFocus />
+                            <button id="shelter_search_button" type="submit" className="btn"><i className="bi bi-search"></i></button>
                         </div>
-                    </div>
+                        <div className="form-group row justify-content-center py-2">
+                            <label className="col-12 col-md-auto my-auto">Filtrar por barrio porteño:</label>
+                            <div className="col-7 col-md-4">
+                                <select id="barrio" name="barrio" className="form-select" aria-label="Seleccionar barrio porteño">
+                                    <option value="Todos" defaultValue>Todos</option>
+                                    { barrios.map((barrio, index) =>
+                                        <option value={barrio} key={index}>{barrio}</option>
+                                    )}
+                                </select>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- src/components/refugio/buscar/BuscadorDeRefugios.jsx
+++ src/components/refugio/buscar/BuscadorDeRefugios.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import { BarraDeBusqueda } from './BarraDeBusqueda';
 import { Resultados } from './Resultados';
 
 const BuscadorDeRefugios = () => {
+    const [ resultados, setResultados ] = useState(false); // primero lo seteo a 'false' para que no muestre nada al cargar la página
+    
     return (
         <div id="buscador_de_refugios">
-            <BarraDeBusqueda />
-            <Resultados />
+            <BarraDeBusqueda setResultados={setResultados} />
+            {resultados && <Resultados resultados={resultados} />}
         </div>
     );
 }
